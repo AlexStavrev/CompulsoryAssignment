94fc43eae408504882294a723f04d9d23b26a29c
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-02-28 12:44:49 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataAccess;
+
+Console.WriteLine("Hello, World!");
+
+string connectionString = "";
+DepartmentRepository repository = new DepartmentRepository(connectionString);
