a93178833ec2a971610990decea6ae5f257d1a0e
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-03-02 09:51:12 +0100

    Added the data access class

---
+++ DataAccess/DepartmentRepository.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using DataAccess.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DataAccess;
+public class DepartmentRepository
+{
+	private readonly SqlConnection _connection;
+
+	public DepartmentRepository(string connectionString)
+	{
+        _connection = new SqlConnection(connectionString);
+    }
+
+	public async Task<int> CreateDepartment(string DName, int MgrSSN)
+	{
+        var parameters = new DynamicParameters();
+        parameters.Add("@DName", DName);
+        parameters.Add("@MgrSSN", MgrSSN);
+        parameters.Add("@NewDeptID", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+        await _connection.ExecuteAsync("usp_CreateDepartment", parameters, commandType: CommandType.StoredProcedure);
+
+        return parameters.Get<int>("@NewDeptID");
+    }
+
+
+    public async Task<bool> UpdateDepartmentName(int DNumber, string DName)
+	{
+        return await _connection.ExecuteAsync("usp_UpdateDepartmentName", new { DNumber, DName }, commandType: CommandType.StoredProcedure) > -1;
+    }
+
+    public async Task<bool> UpdateDepartmentManager(int DNumber, int MgrSSN)
+	{
+        return await _connection.ExecuteAsync("usp_UpdateDepartmentManager", new { DNumber, MgrSSN }, commandType: CommandType.StoredProcedure) > -1;
+    }
+
+
+    public async Task<bool> DeleteDepartment(int DNumber)
+	{
+        return await _connection.ExecuteAsync("usp_DeleteDepartment", new { DNumber }, commandType: CommandType.StoredProcedure) > -1;
+	}
+
+    public async Task<Department> GetDepartment(int DNumber)
+	{
+        return await _connection.QuerySingleOrDefaultAsync<Department>("usp_GetDepartment", new { DNumber }, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<IEnumerable<Department>> GetAllDepartments()
+	{
+		return await _connection.QueryAsync<Department>("usp_GetAllDepartments", commandType: CommandType.StoredProcedure);
+    }
+}
---
+++ DataAccess/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess.Models;
+public record Department
+{
+    public string DName { get; set; }
+    public int DNumber { get; set; }
+    public int MgrSSN { get; set; }
+    public DateTime MgrStartDate { get; set; }
+    public int? EmpCount { get; set; }
+
+    public override string ToString()
+    {
+        return $"Department: {DName} (#{DNumber})\nManager: {MgrSSN} (since {MgrStartDate.ToShortDateString()})\nTotal employees: {EmpCount}";
+    }
+}
